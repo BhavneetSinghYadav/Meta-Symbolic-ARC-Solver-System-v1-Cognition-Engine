1003bd449d0f758fe7a654c846f4aafe5aed94b2
BhavneetSinghYadav <bhavneet.singh.yadav@gmail.com>
2025-06-06 12:19:22 +0530

    Add layout rule detection and injection

--- arc_solver/src/utils/__init__.py
+++ arc_solver/src/utils/__init__.py
@@ -1,5 +1,13 @@
 from .signature_extractor import extract_task_signature, similarity_score
 from .grid_utils import validate_grid
 from .coverage import rule_coverage
+from .patterns import detect_mirrored_regions, detect_repeating_blocks
 
-__all__ = ["extract_task_signature", "similarity_score", "validate_grid", "rule_coverage"]
+__all__ = [
+    "extract_task_signature",
+    "similarity_score",
+    "validate_grid",
+    "rule_coverage",
+    "detect_mirrored_regions",
+    "detect_repeating_blocks",
+]
