eef200589e4363666549c9ef0e59b49b1bc9ba07
BhavneetSinghYadav <bhavneet.singh.yadav@gmail.com>
2025-07-09 07:30:30 +0530

    feat: log skipped rule diagnostics

--- arc_solver/simulator/__init__.py
+++ arc_solver/simulator/__init__.py
@@ -1,1 +1,15 @@
 from .lineage import ColorLineageTracker
+from .logging import (
+    rule_failures_log,
+    log_rule_failure,
+    summarize_skips_by_type,
+    export_failures_json,
+)
+
+__all__ = [
+    "ColorLineageTracker",
+    "rule_failures_log",
+    "log_rule_failure",
+    "summarize_skips_by_type",
+    "export_failures_json",
+]
