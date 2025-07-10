79b9244f5b5eebd930819f15d266c833497e481d
BhavneetSinghYadav <bhavneet.singh.yadav@gmail.com>
2025-07-10 17:54:45 +0530

    Add color lineage validator

--- arc_solver/tests/test_validator.py
+++ arc_solver/tests/test_validator.py
@@ -1,6 +1,9 @@
 import pytest
 from arc_solver.src.core.grid import Grid
-from arc_solver.src.executor.validator import validate_color_dependencies
+from arc_solver.src.executor.validator import (
+    validate_color_dependencies,
+    validate_color_lineage,
+)
 from arc_solver.src.symbolic.vocabulary import Symbol, SymbolType, SymbolicRule, Transformation, TransformationType
 from arc_solver.src.symbolic.rule_language import CompositeRule
 
@@ -23,3 +26,30 @@ def test_validate_color_dependencies_fail():
     grid = Grid([[1, 2]])
     chain = [_rule(1, 3), _rule(3, 3)]
     assert not validate_color_dependencies(chain, grid)
+
+
+def test_validate_color_lineage_recolor_restore():
+    grid = Grid([[1]])
+    comp = CompositeRule([_rule(1, 2), _rule(2, 1)])
+    assert validate_color_lineage(comp, grid)
+
+
+def test_validate_color_lineage_erase_restore():
+    grid = Grid([[1]])
+    step1 = _rule(1, 0)
+    step2 = _rule(0, 1)
+    comp = CompositeRule([step1, step2])
+    assert validate_color_lineage(comp, grid)
+
+
+def test_validate_color_lineage_copy_paste_restore():
+    grid = Grid([[1]])
+    translate = SymbolicRule(
+        transformation=Transformation(
+            TransformationType.TRANSLATE, params={"dx": "0", "dy": "0"}
+        ),
+        source=[],
+        target=[],
+    )
+    comp = CompositeRule([_rule(1, 2), translate, _rule(2, 1)])
+    assert validate_color_lineage(comp, grid)
